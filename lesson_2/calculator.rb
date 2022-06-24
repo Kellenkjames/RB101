c36b38a28c94aa711314ae7ecbb05478802f0a56
Kellen James <Kellenkjames@gmail.com>
2022-06-24 16:23:30 -0400

    rewrites program from scratch for practice

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -15,29 +15,27 @@
 
 Kernel.puts("Welcome to Calculator!")
 
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp() 
+Kernel.puts("Please enter the first number.")
+number1 = Kernel.gets().chomp()
 
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp() 
+Kernel.puts("Please enter the second number.")
+number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 
-  1) add 
-  2) subtract 
-  3) multiply 
-  4) divide"
-)
-operator = Kernel.gets().chomp()
+Kernel.puts("What type of operation to perform: 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp
 
-# The if block does "not" establish a new block scope
 if operator == '1'
-  result =  number1.to_i() + number2.to_i()
+  result = number1.to_i() + number2.to_i()
 elsif operator == '2'
   result = number1.to_i() - number2.to_i()
 elsif operator == '3'
   result = number1.to_i() * number2.to_i()
-else 
+else
   result = number1.to_f() / number2.to_f()
 end 
 
-Kernel.puts("The result is #{result}") 
+Kernel.puts("The result is #{result}")
+
+
+
+
