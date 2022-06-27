fc476e6f99b7e241cfe2071f0db4b641e7489005
Kellen James <Kellenkjames@gmail.com>
2022-06-27 11:52:30 -0400

    resolves all warnings and errors from rubocop linter

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,13 +1,14 @@
-# Calculator + Refactor 
+# Calculator + Refactor
 
-=begin 
+=begin
 
   * Requirements:
 
   TODO => Build a command line calculator program that does the following:
     - asks for two numbers
-    - asks for the type of operation to perform: add, subtract, multiply or divide
-    - displays the result 
+    - asks for the type of operation to perform
+      - add, subtract, multiply, or divide
+    - displays the result
 
 =end
 
@@ -16,50 +17,50 @@ def prompt(message)
 end
 
 def valid_number?(num)
-  num.to_i != 0 
+  num.to_i != 0
 end
 
 def operation_to_message(op)
   case op
   when '1'
-        'Adding'
+    'Adding'
   when '2'
-        'Subtracting'
+    'Subtracting'
   when '3'
-        'Multiplying'
+    'Multiplying'
   when '4'
-        'Dividing'
-  end 
-end 
+    'Dividing'
+  end
+end
 
 prompt("Welcome to Calculator! Enter your name:")
 
-name = nil 
-loop do 
+name = nil
+loop do
   name = gets.chomp
   name.empty? ? prompt("Make sure to use a valid name.") : break
-end 
+end
 
 prompt("Hi #{name}!")
 
 loop do # main loop
-  number1 = nil 
-  loop do 
+  number1 = nil
+  loop do
     prompt("What's the first number?")
     number1 = gets.chomp
     break if valid_number?(number1)
-    
-    prompt("Hmm... that doesn't look like a valid number")
-  end 
 
-  number2 = nil 
-  loop do 
+    prompt("Hmm... that doesn't look like a valid number")
+  end
+
+  number2 = nil
+  loop do
     prompt("What's the second number?")
     number2 = gets.chomp
     break if valid_number?(number2)
-    
+
     prompt("Hmm... that doesn't look like a valid number")
-  end 
+  end
 
   operator_prompt = <<-MSG
     What operation would you like to perform? 
@@ -68,40 +69,36 @@ loop do # main loop
     3) multiply
     4) divide 
   MSG
-  
+
   prompt(operator_prompt)
 
-  operator = nil 
-  loop do 
+  operator = nil
+  loop do
     operator = gets.chomp
-    break if %w(1 2 3 4).include?(operator) 
-    
+    break if %w(1 2 3 4).include?(operator)
+
     prompt("Must choose 1, 2, 3, or 4")
-  end 
+  end
 
   prompt("#{operation_to_message(operator)} the two numbers...")
 
-  result = case operator
-                  when '1'
-                    number1.to_i + number2.to_i
-                  when '2'
-                    number1.to_i - number2.to_i
-                  when '3'
-                    number1.to_i * number2.to_i
-                  when '4'
-                    number1.to_f / number2.to_f
-  end 
+  result =
+    case operator
+    when '1'
+      number1.to_i + number2.to_i
+    when '2'
+      number1.to_i - number2.to_i
+    when '3'
+      number1.to_i * number2.to_i
+    when '4'
+      number1.to_f / number2.to_f
+    end
 
   prompt("The result is #{result}")
 
   prompt("Do you want to perform another calculation? (Y to calculate again)")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-end 
+end
 
 prompt("Thank you for using the calculator. Good bye!")
-
-
-
-
-
