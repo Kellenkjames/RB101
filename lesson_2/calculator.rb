fd8219b610a7b0dfa55234467598f84624595d8d
Kellen James <Kellenkjames@gmail.com>
2022-06-28 14:45:33 -0400

    checkpoint - in process of resolving scope issue

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -14,6 +14,7 @@ MESSAGES = YAML.load_file('config.yml')
 
 =end
 
+# Method for prompts
 def prompt(message)
   puts("=> #{message}")
 end
@@ -30,67 +31,67 @@ def number?(num_string)
 end
 
 # Language translation
-def lang(langauge)
-  langauge == "en" ? "en" : "es"
+def translation(lang)
+  lang == 'en' ? 'en' : 'es'
 end 
 
 def operation_to_message(op)
   case op
   when '1'
-    return MESSAGES['en']['adding']
+    return MESSAGES["#{translation(lang)}"]['adding']
   when '2'
-    return MESSAGES['en']['subtracting']
+    return MESSAGES["#{translation(lang)}"]['subtracting']
   when '3'
-    return MESSAGES['en']['multiplying']
+    return MESSAGES["#{translation(lang)}"]['multiplying']
   when '4'
-    return MESSAGES['en']['dividing']
+    return MESSAGES["#{translation(lang)}"]['dividing']
   end
   puts "Now code can be run safely after case statement"
 end
 
-langauge = nil 
+lang = nil 
 loop do 
   puts "Do you want to run calculator in english or spanish? (en for english, es for spanish)"
-  langauge = gets.chomp
+  lang = gets.chomp
   
-  break if langauge == "en" || langauge == "es"
+  break if lang == "en" || lang == "es"
   puts "Please type en for english or es for spanish"
 end
 
-prompt(MESSAGES["#{lang(langauge)}"]['welcome'])
+prompt(MESSAGES["#{translation(lang)}"]['welcome'])
 
 name = nil
 loop do
   name = gets.chomp
-  name.empty? ? prompt(MESSAGES['en']['valid_name']) : break
+  name.empty? ? prompt(MESSAGES["#{translation(lang)}"]['valid_name']) : break
 end
 
-prompt("#{MESSAGES['en']['greeting']} #{name}!")
+prompt("#{MESSAGES["#{translation(lang)}"]['greeting']} #{name}!")
 
 loop do # main loop
   number1 = nil
   loop do
-    prompt(MESSAGES['en']['first_number'])
+    prompt(MESSAGES["#{translation(lang)}"]['first_number'])
     number1 = gets.chomp
     break if number?(number1)
 
-    prompt(MESSAGES['en']['invalid_number'])
+    prompt(MESSAGES["#{translation(lang)}"]['invalid_number'])
   end
 
   number2 = nil
   loop do
-    prompt(MESSAGES['en']['second_number'])
+    prompt(MESSAGES["#{translation(lang)}"]['second_number'])
     number2 = gets.chomp
     break if number?(number2)
 
-    prompt(MESSAGES['en']['invalid_number'])
+    prompt(MESSAGES["#{translation(lang)}"]['invalid_number'])
   end
 
-  operator_prompt = "#{MESSAGES['en']['operation']}
-    #{MESSAGES['en']['add']}
-    #{MESSAGES['en']['subtract']}
-    #{MESSAGES['en']['multiply']}
-    #{MESSAGES['en']['divide']}"
+  operator_prompt = "#{MESSAGES["#{translation(lang)}"]['operation']}
+    #{MESSAGES["#{translation(lang)}"]['add']}
+    #{MESSAGES["#{translation(lang)}"]['subtract']}
+    #{MESSAGES["#{translation(lang)}"]['multiply']}
+    #{MESSAGES["#{translation(lang)}"]['divide']}"
 
   prompt(operator_prompt)
 
@@ -99,10 +100,10 @@ loop do # main loop
     operator = gets.chomp
     break if %w(1 2 3 4).include?(operator)
 
-    prompt(MESSAGES['en']['must_choose'])
+    prompt(MESSAGES["#{translation(lang)}"]['must_choose'])
   end
 
-  prompt("#{operation_to_message(operator)} #{MESSAGES['en']['two_numbers']}")
+  prompt("#{operation_to_message(operator)} #{MESSAGES["#{translation(lang)}"]['two_numbers']}")
 
   result =
     case operator
@@ -116,11 +117,11 @@ loop do # main loop
       number1.to_f / number2.to_f
     end
 
-  prompt("#{MESSAGES['en']['result']} #{result}")
+  prompt("#{MESSAGES["#{translation(lang)}"]['result']} #{result}")
 
-  prompt(MESSAGES['en']['another_calc?'])
+  prompt(MESSAGES["#{translation(lang)}"]['another_calc?'])
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt(MESSAGES['en']['thank_you'])
+prompt(MESSAGES["#{translation(lang)}"]['thank_you'])
