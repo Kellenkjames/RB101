30956966d04241e3a4015cb2016fd972f47e1fd9
Kellen James <Kellenkjames@gmail.com>
2022-06-28 17:32:43 -0400

    logic added for handling loan amount

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,8 +1,14 @@
 # Mortgage Calculator
 
+# Handle prompts
 def prompt(msg)
   puts ">> #{msg}"
 end 
+
+# Handle number validation
+def valid_num?(input)
+  input.to_i.to_s == input && input.to_i > 0
+end
 
 name = nil 
 loop do 
@@ -13,4 +19,13 @@ end
 
 prompt("Welcome in #{name}. Let's calculate your mortgage.")
 
+loan_amount = nil 
+loop do 
+  prompt("What is the loan amount")
+  loan_amount = gets.chomp
 
+  break if valid_num?(loan_amount)
+  prompt("Please enter a valid number")
+end 
+
+prompt("The loan amount is #{loan_amount}")
