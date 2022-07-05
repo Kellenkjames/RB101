eaf7b63a6f8d72fab83f3615d4dcda46486b4d29
Kellen James <Kellenkjames@gmail.com>
2022-07-05 10:09:23 -0400

    checkpoint - adds logic for score counter + game reset

--- lesson_2/small_programs/rock_paper_scissors.rb
+++ lesson_2/small_programs/rock_paper_scissors.rb
@@ -2,24 +2,27 @@
 
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
+$player_score = 0
+$computer_score = 0  
+
 def prompt(message)
   puts ">> #{message}"
 end
 
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  (first == 'paper' && second == 'rock') ||
+  (first == 'scissors' && second == 'paper')
 end 
 
 def more_options?(first, second)
   (first == 'rock' && second == 'lizard') || 
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'spock' && second == 'scissors') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'lizard' && second == 'paper') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'spock' && second == 'rock') 
+  (first == 'lizard' && second == 'spock') ||
+  (first == 'spock' && second == 'scissors') ||
+  (first == 'scissors' && second == 'lizard') ||
+  (first == 'lizard' && second == 'paper') ||
+  (first == 'paper' && second == 'spock') ||
+  (first == 'spock' && second == 'rock') 
 end 
 
 def word_shorten(word) 
@@ -48,16 +51,23 @@ end
 
 # Each time the function is called (score is reset back to "zero") --> How do we fix this?
 def score_counter(player, computer) 
-  player_score = 0
-  computer_score = 0  
-  
   if win?(player, computer) || more_options?(player, computer)
-    player_score += 1
+    $player_score += 1
   elsif win?(computer, player) || more_options?(computer, player) 
-    computer_score += 1
+    $computer_score += 1
   end
-  prompt("Your score: #{player_score} points")
-  prompt("Computer score: #{computer_score} points")
+  prompt("Your score: #{$player_score} points")
+  prompt("Computer score: #{$computer_score} points")
+end
+
+def game_reset(player_score, computer_score) 
+  if player_score == 3 
+    puts("Match is over. You win!")
+  elsif computer_score == 3 
+    puts("Match is over. Computer wins!")
+  end 
+  $player_score = 0
+  $computer_score = 0
 end
 
 loop do
@@ -73,16 +83,18 @@ loop do
     break if VALID_CHOICES.include?(word)
     prompt("That's not a valid choice")
   end
-
+  
   computer_choice = VALID_CHOICES.sample
-
+  
   prompt("You chose: #{word} | Computer chose: #{computer_choice}")
   display_results(word, computer_choice)
   score_counter(word, computer_choice)
-
+  
+  game_reset($player_score, $computer_score)
+  
   prompt('Do you want to play again? y for another round')
   answer = gets.chomp.downcase
-
+  
   break unless answer.include?('y')
 end
 
