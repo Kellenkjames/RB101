4a982c039168dd26f626d6af5c3a76849405ed7b
Kellen James <Kellenkjames@gmail.com>
2022-07-07 14:36:14 -0400

    completes first medium 1 problem

---
+++ lesson_3/practice_problems/medium/pset1.rb
@@ -0,0 +1,15 @@
+#* Practice Problems: Medium 1
+
+# Question 1: Write a one-line program that creates the following output 10 times, with the subsequent line indented 1 space to the right: 
+
+msg = 'The Flintstones Rock!'
+spaces = msg.length
+
+count = 0 
+
+(puts msg.rjust(spaces); spaces +=1; count +=1;) while count < 10
+
+
+
+
+
