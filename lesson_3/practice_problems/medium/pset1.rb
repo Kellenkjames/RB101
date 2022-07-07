32bb13e4d812c60236eed60273c7844d9a69dd85
Kellen James <Kellenkjames@gmail.com>
2022-07-07 16:18:40 -0400

    completes question 2 (TypeError)

--- lesson_3/practice_problems/medium/pset1.rb
+++ lesson_3/practice_problems/medium/pset1.rb
@@ -1,14 +1,22 @@
 #* Practice Problems: Medium 1
 
-# Question 1: Write a one-line program that creates the following output 10 times, with the subsequent line indented 1 space to the right: 
+# Question 1: "ASCII Art" Write a one-line program that creates the following output 10 times, with the subsequent line indented 1 space to the right: 
 
-msg = 'The Flintstones Rock!'
-spaces = msg.length
+10.times { |number| puts (" " * number) + 'The Flinstones Rock!' }
 
-count = 0 
+# ----------------------------------------------------------------
 
-(puts msg.rjust(spaces); spaces +=1; count +=1;) while count < 10
+# Question 2: The result of the following statement will be an error: 
 
+# puts "the value of 40 + 2 is " + (40 + 2)
+
+# Why is this: This will raise an error TypeError: no implicit conversion of Integer into String because (40+2) results in an integer and it is being concatenated to a string.
+
+#* One possible way to fix: String interpolation 
+puts "the value of 40 + 2 is #{40 + 2}"
+
+#* Second possible solution: Use .to_s method 
+puts "the value of 40 + 2 is " + (40 + 2).to_s
 
 
 
