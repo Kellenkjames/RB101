c20c8b58e04937043c17243d2d80d879a06969ed
Kellen James <Kellenkjames@gmail.com>
2022-07-07 12:01:31 -0400

    checkpoint, completes age hash merge problem

--- lesson_3/practice_problems/easy/pset2.rb
+++ lesson_3/practice_problems/easy/pset2.rb
@@ -33,4 +33,19 @@ p munsters_description.upcase!
 
 # ----------------------------------------------------------------
 
+# Question 3: We have most of the Munster family in our age hash: 
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+
+# Add ages for Marilyn and Spot to the existing hash 
+
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+
+p ages.merge!(additional_ages)
+
+# ----------------------------------------------------------------
+
+
+
+
 
