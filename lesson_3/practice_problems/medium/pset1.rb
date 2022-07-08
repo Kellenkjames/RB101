472664229a732222cfee1498b5d8446d75d29d05
Kellen James <Kellenkjames@gmail.com>
2022-07-08 14:18:05 -0400

    completes remaining medium exercises

--- lesson_3/practice_problems/medium/pset1.rb
+++ lesson_3/practice_problems/medium/pset1.rb
@@ -142,3 +142,45 @@ Spot's demo_hash is pointing to the 'munsters' hash on line 119. In this case, t
 #* The actual hash object that is being messed with inside of the method IS the munsters hash. 
 
 =end 
+
+# ----------------------------------------------------------------
+
+# Question 8: Method calls can take expressions as arguments. Suppose we define a method called rps as follows, which follows the classic rules of rock-paper-scissors game: it returns the winning fist or, in the case of a tie, the fist that both players played.
+
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+# What is the result of the following call? 
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock") # paper 
+
+# ----------------------------------------------------------------
+
+# Question 9: Consider these two simple methods: 
+
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+# What would be the return value of the following method invocation? 
+puts bar(foo) # no
+
+#* Solution: This is because the value returned from the 'foo' method will always be "yes", and "yes" == "no" will be false. 
+
+
+
+
+
+
+
+
