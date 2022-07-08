f0224980590a3ba8b9361b8d7e319655bf71ab0a
Kellen James <Kellenkjames@gmail.com>
2022-07-08 14:02:58 -0400

    checkpoint completes question 7 and 8

--- lesson_3/practice_problems/medium/pset1.rb
+++ lesson_3/practice_problems/medium/pset1.rb
@@ -102,7 +102,43 @@ end
 
 new_answer = mess_with_it(answer)
 
-p answer - 8 # 34 because the method never gets "called" it only gets "stored"
+p answer - 8 # 34  
 
+# ----------------------------------------------------------------
 
+# Question 7: One day Spot was playing with the Munster family's home computer and he wrote a small program to mess with their demographic data:
 
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+puts mess_with_demographics(munsters)
+
+# Did the family's data get ransacked? Why or why not? 
+
+#* Answer: Yes 
+
+=begin
+
+# Explanation 
+
+Remember that in Ruby, what gets passed to a method isn't the value of the object. Under the hood, Ruby passes the object_id of each argument to the method. 
+
+The method stores these object_id's internally in locally scoped (private to the method) variables (named per the method definition's parameter list).
+
+Spot's demo_hash is pointing to the 'munsters' hash on line 119. In this case, the program does not create a new hash for the result -- it just uses the existing hash as-is. 
+
+#* The actual hash object that is being messed with inside of the method IS the munsters hash. 
+
+=end 
