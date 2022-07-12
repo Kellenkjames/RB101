8b789c7b42a14403b9aba66c7407ea838ff1ef93
Kellen James <Kellenkjames@gmail.com>
2022-07-12 11:28:12 -0400

    adds break statement examples

--- lesson_4/looping.rb
+++ lesson_4/looping.rb
@@ -58,3 +58,59 @@ loop do
 end 
 
 # --------------------------------------
+
+#* Iteration 
+
+# Although we have an if condition for break, we still don't know how many times the loop will iterate because number represents a random number. We can tell loop to iterate a specific number of times by using a variable that tracks the number of iterations performed. Before loop is implemented, let's assign a variable counter that represents the current iteration number.
+
+# In order to align the value of counter and the number of iterations, we need to increment counter by 1 during each iteration. This will ensure that loop only iterates five times.
+
+counter = 0 
+
+loop do 
+  puts 'Hello!'
+  counter += 1
+  break if counter == 5
+end 
+
+#* Notice that counter is initialized outside of the loop. If we had initialized it inside of the loop it would be reassigned to 0 on each iteration and the break condition would never be met. 
+
+# --------------------------------------
+
+#* if modifier 
+
+# Here we shortened the if statement by changing it to an if modifier. An if modifier is implemented by appending the keyword if and the condition to a statement. In this case, the statement is break. 
+
+break if number == 5 
+
+# This generally makes the code more readable but should only be used when applicable. If there are multiple lines of code within the if statement then a modifier can't be used (notice we removed the puts ' Exiting...' line).
+
+# --------------------------------------
+
+#* Break Placement 
+
+# This example of loop - where break is placed on the last line within the loop - mimics the behavior of a "do/while" loop. With a "do/while" loop, the code within the block is guaranteed to execute at least once.
+
+counter = 0 
+
+loop do 
+  puts 'Hello!'
+  counter += 1
+  break if counter == 5
+end 
+
+# If we move break to the first line within the loop, this will then mimic the behavior of a while loop. The while keyword always precedes a condition. If the condition evaluates as true, the loop will execute the code within the block. If the condition evaluates as false, the loop will stop. This means the code below break may or may not execute at all, depending on the condition.
+
+counter = 0 
+
+loop do 
+  break if counter == 0
+  puts 'Hello!'
+  counter += 1
+end
+
+# In our example, if we move break to the first line within the loop and change the condition to counter == 0, then the loop will stop immediately and not execute any code after break.
+
+# --------------------------------------
+
+
