bdbed7a22c2244adab85ace25046e691285201a4
Kellen James <Kellenkjames@gmail.com>
2022-07-13 18:14:36 -0400

    completes conditional_loop exercise

--- lesson_4/loops_two/conditional_loop.rb
+++ lesson_4/loops_two/conditional_loop.rb
@@ -6,3 +6,14 @@
 
 # When process_the_loop is false, just print The loop wasn't processed!.
 
+process_the_loop = [true, false].sample
+
+if process_the_loop
+  loop do 
+    puts "The loop was processed"
+    break 
+  end 
+else
+  puts "The loop wasn't processed!"
+end 
+
