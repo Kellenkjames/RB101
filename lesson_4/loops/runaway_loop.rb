a2f32c005109651f4e8307b1d1185d0c579cdd5c
Kellen James <Kellenkjames@gmail.com>
2022-07-13 14:54:16 -0400

    completes runaway_loop problem

--- lesson_4/loops/runaway_loop.rb
+++ lesson_4/loops/runaway_loop.rb
@@ -3,6 +3,6 @@
 # The code below is an example of an infinite loop. The name describes exactly what it does: loop infinitely. This loop isn't useful in a real program, though. Modify the code so the loop stops after the first iteration.
 
 loop do
-  break 
   puts 'Just keep printing...'
+  break 
 end
