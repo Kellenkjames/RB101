512fe32d84958d0d89e45c761726029f66db17f7
Kellen James <Kellenkjames@gmail.com>
2022-07-16 19:05:47 -0400

    checkpoint - adds problem sets for transformations

--- lesson_4/selection_transformation.rb
+++ lesson_4/selection_transformation.rb
@@ -14,14 +14,81 @@ counter = 0
 
 loop do 
   current_char = alphabet[counter]
-
+  
   if current_char == 'g'
     selected_chars << current_char # appends current_char into the selected_chars string 
   end 
-
+  
   counter += 1
   break if counter == alphabet.size
 end 
 
 selected_chars # => "g"
+
+# The if condition is what determines which values are selected and which ones are ignored; this is the selection criteria. The rest of the code is just your basic loop iterating code. 
+
+#----------------------------------------------
+
+#* Transformations 
+
+fruits = ['apple', 'banana', 'pear']
+transformed_elements = []
+counter = 0
+
+loop do 
+  current_element = fruits[counter]
+  
+  transformed_elements << current_element + 's' # appends transformed string into array 
+  
+  counter += 1
+  break if counter == fruits.size 
+  
+end 
+
+transformed_elements # => ['apples', 'bananas', 'pears']
+
+# Since we're applying the transformation to every element in the array, we don't need an if condition, but the entire line is the transformation criteria.
+
+# Note that, in this example, we perform the transformation on a new array and leave the original array unchanged. 
+
+#* When performing transformation, it's always important to pay attention to whether the original collection was mutated or if a new collection was returned.
+
+#----------------------------------------------
+
+#* Extracting Methods 
+
+# Most of the time, selecting or transforming a collection is a very specific action -- select all the odd numbers, turn all elements into strings, etc -- which naturally lends the specific action being extracted into convenience methods.
+
+# Ex) Let's say we wanted to extract all the vowels in a given string. We're going to use the helpful String#include? method here to assist us in determining whether a character is a vowel. 
+
+=begin
+
+* if 'aeiouAEIOU'.include?(current_char)
+    selected_chars << current_char
+  end
+
+=end 
+
+# Let's now put it all together into a select_vowels method so we can call this method on any string.
+
+def select_vowels(str)
+  selected_chars = ''
+  counter = 0
+  
+  loop do 
+    current_char = str[counter]
+    
+    if 'aeiouAEIOU'.include?(current_char)
+      selected_chars << current_char
+    end
+    
+    counter += 1
+    break if counter = str.size
+  end 
+  
+  selected_chars
+end
+
+
+
 
