23971763d41130d36ec073b6f1f38b82b9e62da4
Kellen James <Kellenkjames@gmail.com>
2022-07-20 16:18:46 -0400

    completes final practice_problem exercise

--- lesson_4/practice_problems.rb
+++ lesson_4/practice_problems.rb
@@ -139,3 +139,23 @@ Second, where did that nil come from? If we look at the if condition (value.size
 * When none of the conditions in an if statement evaluates as true, the if statement itself returns nil. That's why we see nil as the first element in the returned array.
 
 =end 
+
+# 10: What is the return value of the following code? Why? 
+
+[1, 2, 3].map do |num|
+  if num > 1
+    puts num 
+  else 
+    num
+  end
+end
+
+# => [1, nil, nil]
+
+=begin
+
+We can determine the block's return value by looking at the return value of the last statement evaluated within the block. In this case, it's a bit tricky because of the if statement. 
+
+#* For the first element, the if condition evaluates as false, which means num is the block's return value for that iteration. For the rest of the elements in the array, num > 1 evaluates as true, which means puts num is the last statement evaluated, which in turn, means that the block's return value is nil for those iterations. 
+
+=end 
