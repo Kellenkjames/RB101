6afba1d2d3be5248abf927c54aa62bbed9a71aaa
Kellen James <Kellenkjames@gmail.com>
2022-07-22 15:36:26 -0400

    cleans up methods in additional practice problems

--- lesson_4/additional_practice.rb
+++ lesson_4/additional_practice.rb
@@ -61,7 +61,7 @@ be_index # => 3
 
 # or 
 
-flintstones.index { |name| name[0, 2] == "Be" }
+flintstones.index { |name| name[0, 2] == "Be" } # ary[start, length]
 
 # --------------------------------------
 
@@ -138,16 +138,22 @@ Iteration 3: Starts from index 2
 
 def titleize(words)
   arr = words.split(' ')
-  arr.each { |word| word.capitalize! }
-  arr.join(" ")
+  arr.each { |word| word.capitalize! }.join(' ')
 end 
 
 words = "the flintstones rock"
-titleize(words) # => "The Flintstones Rock"
+puts titleize(words) # => "The Flintstones Rock"
 
 # --------------------------------------
 
 # 10: Modify the hash such that each member of the Munster family has an additional "age_group" key that has one of three values describing the age group the family member is in (kid, adult, or senior). Your solution should produce the hash below:
+
+#* Expected Output:
+# { "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
+#   "Lily" => {"age" => 30, "gender" => "female", "age_group" => "adult" },
+#   "Grandpa" => { "age" => 402, "gender" => "male", "age_group" => "senior" },
+#   "Eddie" => { "age" => 10, "gender" => "male", "age_group" => "kid" },
+#   "Marilyn" => { "age" => 23, "gender" => "female", "age_group" => "adult" } }
 
 munsters = {
   "Herman" => { "age" => 32, "gender" => "male" },
@@ -156,13 +162,6 @@ munsters = {
   "Eddie" => { "age" => 10, "gender" => "male" },
   "Marilyn" => { "age" => 23, "gender" => "female"}
 }
-
-#* Expected Output:
-# { "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
-#   "Lily" => {"age" => 30, "gender" => "female", "age_group" => "adult" },
-#   "Grandpa" => { "age" => 402, "gender" => "male", "age_group" => "senior" },
-#   "Eddie" => { "age" => 10, "gender" => "male", "age_group" => "kid" },
-#   "Marilyn" => { "age" => 23, "gender" => "female", "age_group" => "adult" } }
 
 def age_group(age)
   if age >= 0 && age <= 17
@@ -180,6 +179,19 @@ munsters.each_value do |value|
 end
 puts munsters
 
+# or 
+munsters.each do |name, details|
+  case details["age"]
+  when 0...18
+    details["age_group"] = "kid"
+  when 18...65
+    details["age_group"] = "adult"
+  else
+    details["age_group"] = "senior"
+  end
+end
+
+# --------------------------------------
 
 
 
