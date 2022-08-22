2960e3da30cd685cc5bd09b5cee0b4d504b885e3
Kellen James <Kellenkjames@gmail.com>
2022-08-22 13:03:30 -0400

    completes array sort exercise

---
+++ lesson_5/practice_problems.rb
@@ -0,0 +1,12 @@
+#* Practice Problems: Sorting, Nested Collections and Working with Blocks 
+
+# Problem 1: How would you order this array of number strings by descending numeric value?
+
+arr = ['10', '11', '9', '7', '8']
+
+arr.sort do |a, b|
+  b.to_i <=> a.to_i
+end 
+# => ["11", "10", "9", "8", "7"]
+
+# ------------------------------------------------
