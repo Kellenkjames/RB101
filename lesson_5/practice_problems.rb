668b55e43efdab1f0cd9d0ab2e72a7b5454a17d5
Kellen James <Kellenkjames@gmail.com>
2022-08-23 11:04:04 -0400

    completes problem 7 (final values of a and b)

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -110,6 +110,26 @@ munsters.each_pair do |name, details|
   puts "(#{name}) is a (#{details["age"]})-year-old (#{details[("gender")]})."
 end
 
+# ------------------------------------------------
+
+# Problem 7: What would be the final values of a and b? Try to work this out without running the code. 
+
+a = 2
+b = [5, 8]
+arr = [a, b] 
+# => [2, [5, 8]]
+
+arr[0] += 2 
+arr[1][0] -= a
+
+a     # => 2
+b     # => [3, 8]
+arr   # => [4, [3, 8]]
+
+# ------------------------------------------------
+
+
+
 
 
 
