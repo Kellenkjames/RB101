ff8cee04d95a7a15588768d8c3346a1e7f4747a9
Kellen James <Kellenkjames@gmail.com>
2022-08-23 15:39:23 -0400

    completes data structure (increment) exercise

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -155,6 +155,17 @@ arr.map do |sub_array|
 end 
 # => [["c", "b", "a"], [3, 2, 1], ["green", "blue", "black"]]
 
+# ------------------------------------------------
+
+# Problem 10: Given the following data structure and without modifying the original array, use the map method to return a new array identical in structure to the original but where the value of each integer is incremented by 1.
+
+[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |hsh|
+  incremented_hsh = {}
+  hsh.each do |key, value|
+    incremented_hsh[key] = value + 1
+  end 
+  incremented_hsh
+end 
 
 
 
