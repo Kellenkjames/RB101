5902b41f539d37053574078fd1b7411f6263c2cd
Kellen James <Kellenkjames@gmail.com>
2022-08-23 19:32:12 -0400

    checkpoint, array sorting updates

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -191,3 +191,16 @@ arr.each do |sub_arr|
 end 
 hsh # => {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
 
+# ------------------------------------------------
+
+# Problem 13: Given the following data structure, return a new array containing the same sub-arrays as the original but ordered logically by only taking into consideration the odd numbers they contain.
+
+arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
+
+#       [[1, 8, 3], [1, 5, 9], [6, 1, 7], [1, 6, 9]]
+
+arr.sort do |a, b|
+    a <=> b
+end
+
+
