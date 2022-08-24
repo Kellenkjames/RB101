cc3d99ce3c611e50ffb7fbc2241793f21850ebb0
Kellen James <Kellenkjames@gmail.com>
2022-08-24 14:51:07 -0400

    completes problem 15 (even integers)

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -230,3 +230,20 @@ hsh.map do |_, details|
 end 
 
 # => [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+
+# ------------------------------------------------
+
+# Problem 15: Given this data structure write some code to return an array which contains only the hashes where all the integers are even.
+
+arr = [
+  {a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, 
+  {e: [8], f: [6, 10]}
+]
+
+arr.select do |hsh|
+  hsh.all? do |_, value|
+    value.all? do |num|
+      num.even? 
+    end 
+  end 
+end 
