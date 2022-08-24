be2fa8feeef6d4d13093a83f35bc5aa1c42ba60a
Kellen James <Kellenkjames@gmail.com>
2022-08-24 10:42:03 -0400

    completes exercise 13 data structure

--- lesson_5/practice_problems.rb
+++ lesson_5/practice_problems.rb
@@ -197,10 +197,14 @@ hsh # => {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
 
 arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
 
+arr.sort_by do |sub_arr| 
+  sub_arr.select do |num|
+    num.odd? 
+  end
+end 
+
+
 #       [[1, 8, 3], [1, 5, 9], [6, 1, 7], [1, 6, 9]]
 
-arr.sort do |a, b|
-    a <=> b
-end
-
+# By performing selection on the sub-arrays that we are comparing, we can compare them based on the value of the odd integers alone. 
 
