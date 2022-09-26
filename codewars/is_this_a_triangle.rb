779cf0ff3bb442bd1d5db97c40586387fa995895
Kellen James <Kellenkjames@gmail.com>
2022-09-26 12:00:47 -0400

    checkpoint, code refactor to get additional tests passing

--- codewars/is_this_a_triangle.rb
+++ codewars/is_this_a_triangle.rb
@@ -1,4 +1,6 @@
 
+require 'pry'
+
 # Is this a triangle? 
 
 #* Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
@@ -37,15 +39,20 @@ is_triangle(7, 2, 2) == false
 
 * D - Data Structure 
 
-# => N/A 
+# => []
 
 # ------------------------------------------------
 
-* A - Algorithm 
+* A - Algorithm:
 
 Algorithm: 
-  b + c > a ? true : false
-end 
+- the two inputs that have equal values (are the sides)
+- check for inputs that have the same values
+- arr = [a, b , c]
+- arr.select do |num|
+  p num
+  end 
+end
 
 =end 
 
@@ -54,10 +61,20 @@ end
 #* C - Code 
 
 def is_triangle(a, b, c)
-  b + c > a ? true : false 
+  arr = [a, b, c]
+
+  return false if arr[0] < 0 || arr[1] < 0 || arr[2] < 0 
+  
+  if arr[0] + arr[1] > arr[2] && arr[1] + arr[2] > arr[0] && arr[0] + arr [2] > arr[1]
+    true 
+  else 
+    false
+  end 
+
 end 
 
-is_triangle(1, 2, 2)
-is_triangle(7, 2, 2)
-
-
+p is_triangle(1, 2, 2) # true 
+p is_triangle(7, 2, 2) # false 
+p is_triangle(9, 3, 8) # true
+p is_triangle(3, 1, 10) # false
+p is_triangle(2, 7, 4) # false 
