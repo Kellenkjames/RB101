a788eb593603c62ac0feb789c98c42a53292f392
Kellen James <Kellenkjames@gmail.com>
2022-09-26 12:30:40 -0400

    removes reference to pry

--- codewars/is_this_a_triangle.rb
+++ codewars/is_this_a_triangle.rb
@@ -1,6 +1,4 @@
 
-require 'pry'
-
 # Is this a triangle? 
 
 #* Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
