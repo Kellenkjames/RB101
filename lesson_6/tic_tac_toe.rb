0dd87cc668482ab36c22e3d5e13926ea4cb948b4
Kellen James <Kellenkjames@gmail.com>
2022-11-02 15:36:36 -0400

    abstracts ai_defense and ai_offense into separate methods

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -77,20 +77,35 @@ def player_turn!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def immediate_threat(brd)
+# Computer AI: Offense: 
+
+# The defensive minded AI is pretty cool, but it's still not performing as well as it could because if there are no impending threats, it will pick a square at random. We'd like to make a slight improvement on that. We're not going to add in any complicated algorithm (there's an extra bonus below on that), but all we want to do is piggy back on our find_at_risk_square from bonus 3 above and turn it into an attacking mechanism as well. 
+
+# The logic is simple: if the computer already has 2 in a row, then fill in the 3rd square, as opposed to moving at random.
+
+def computer_ai_defense(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 2
       line.each do |square|
         brd[square] = COMPUTER_MARKER if brd[square] == INITIAL_MARKER
       end
-    end
+    end 
   end
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
+end
+
+def computer_ai_offense(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 2
+      line.each do |square|
+        brd[square] = PLAYER_MARKER if brd[square] == INITIAL_MARKER
+      end
+    end 
+  end
 end
 
 def computer_turn!(brd)
-  immediate_threat(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
 end
 
 def board_full?(brd)
@@ -135,6 +150,9 @@ loop do
 
     player_turn!(board)
     break if someone_won?(board) || board_full?(board)
+
+    computer_ai_defense(board)
+    computer_ai_offense(board)
 
     computer_turn!(board)
     break if someone_won?(board) || board_full?(board)
