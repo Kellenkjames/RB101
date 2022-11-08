d57836c9109a9fd493defd9d76d7cafd3e71d4d9
Kellen James <Kellenkjames@gmail.com>
2022-11-08 14:52:22 -0500

    removes trailing whitespace

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -131,25 +131,26 @@ def first_move(brd)
     prompt 'Who should go first 🤔 P => Player | C => Computer'
     answer = gets.chomp.upcase
     options = ['P', 'C']
-    
+  
     if answer == 'P'
       current_player = 'Player'
     elsif answer == 'C'
       current_player = 'Computer'
       prompt 'Computer will choose who goes first:'
-      
+    
       random_selection = options.sample
-      
+    
       if random_selection == 'P'
         prompt 'Player is up.'
         current_player = 'Player'
-      else 
+      else
         prompt 'Computer is up.'
         current_player = 'Computer'
       end
     end
-    
+  
     break if options.include?(answer)
+  
     prompt 'Please select either (P for Player) or (C for Computer).'
   end
 
