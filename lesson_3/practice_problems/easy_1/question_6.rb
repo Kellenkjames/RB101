e3438788dfdbf0426ce5874bab230f8d97ae05de
Kellen James <Kellenkjames@gmail.com>
2023-01-24 16:37:58 -0500

    Add solution for question 6

---
+++ lesson_3/practice_problems/easy_1/question_6.rb
@@ -0,0 +1,11 @@
+# Question 6
+
+# Starting with the string: show two different ways to put the expected "Four score and " in front of it.
+
+famous_words = "seven years ago..."
+
+# first method 
+famous_words.prepend('Four score and ')
+
+# second method
+famous_words.insert(0, 'Four score and ')
