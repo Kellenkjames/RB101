b56cda099743c7cea34fd40d268ed8446b60b7a8
Kellen James <Kellenkjames@gmail.com>
2023-01-24 18:20:18 -0500

    Add insert method to add multiple values

---
+++ lesson_3/practice_problems/easy_2/question_7.rb
@@ -0,0 +1,9 @@
+# Question 7
+
+# In the previous practice problem we added Dino to our array like this:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones << "Dino"
+
+# How can we add multiple items to our array? (Dino and Hoppy)
+flintstones.insert(-1, 'Dino', 'Hoppy')
