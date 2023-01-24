c9e352f4021449a1733b2953f7f055dea7c6c7d1
Kellen James <Kellenkjames@gmail.com>
2023-01-24 16:24:49 -0500

    Add updates for questions 4 & 5

--- lesson_3/practice_problems/easy_1/question_4.rb
+++ lesson_3/practice_problems/easy_1/question_4.rb
@@ -8,7 +8,7 @@ numbers = [1, 2, 3, 4, 5]
 
 numbers.delete_at(1)         # numbers i now [1, 3, 4, 5]
 
-numbers.delete(1)         # numbers i now [2, 3, 4, 5]
+numbers.delete(1)             # numbers i now [2, 3, 4, 5]
 
 =begin
 
